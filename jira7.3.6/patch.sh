609e2cdc88223122eb2022b0565349ffb0b6293b
arthur <yahui9119@163.com>
2018-06-08 13:49:24 +0800

    add path files

---
+++ jira7.3.6/patch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pwd 
+rm -f /opt/atlassian/jira/atlassian-jira/WEB-INF/lib/atlassian-extras-3.2.jar 
+wget -P /opt/atlassian/jira/atlassian-jira/WEB-INF/lib/ https://github.com/YahuiWong/Usefulfiles/raw/master/jira7.3.6/atlassian-extras-3.2.jar
+echo "Patch Done!"
