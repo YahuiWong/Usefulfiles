365efe983033988a6f41ddb250b0fd7c68be928d
arthur <yahui9119@163.com>
2018-06-08 13:56:02 +0800

    fix

--- jira7.3.6/patch.sh
+++ jira7.3.6/patch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-pwd
-rm -f /opt/atlassian/jira/atlassian-jira/WEB-INF/lib/atlassian-extras-3.2.jar
-wget -P /opt/atlassian/jira/atlassian-jira/WEB-INF/lib/ https://github.com/YahuiWong/Usefulfiles/raw/master/jira7.3.6/atlassian-extras-3.2.jar
+pwd|\
+echo "Delete atlassian-extras-3.2.jar!"
+rm -f /opt/atlassian/jira/atlassian-jira/WEB-INF/lib/atlassian-extras-3.2.jar|\
+echo "Get New atlassian-extras-3.2.jar!"
+wget -P /opt/atlassian/jira/atlassian-jira/WEB-INF/lib/ https://github.com/YahuiWong/Usefulfiles/raw/master/jira7.3.6/atlassian-extras-3.2.jar|\
 echo "Patch Done!"
