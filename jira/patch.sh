1a1e1f94618ba4100197e318a4249444b198a77f
arthur <yahui9119@163.com>
2018-09-25 17:05:54 +0800

    add files

--- jira/patch.sh
+++ jira/patch.sh
@@ -3,5 +3,5 @@ pwd|\
 echo "Delete atlassian-extras-3.2.jar!"
 rm -f /opt/atlassian/jira/atlassian-jira/WEB-INF/lib/atlassian-extras-3.2.jar|\
 echo "Get New atlassian-extras-3.2.jar!"
-wget -P /opt/atlassian/jira/atlassian-jira/WEB-INF/lib/ https://github.com/YahuiWong/Usefulfiles/raw/master/jira7.3.6/atlassian-extras-3.2.jar|\
+wget -P /opt/atlassian/jira/atlassian-jira/WEB-INF/lib/ https://github.com/YahuiWong/Usefulfiles/raw/master/jira/atlassian-extras-3.2.jar|\
 echo "Patch Done!"
